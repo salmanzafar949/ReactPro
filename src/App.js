c4ca3ba0fb0487a19cf91da98397679d8cfc2580
Salman Zafar <salman.zafar@innowi.com>
2020-10-07 17:31:02 +0500

    tailwind installation and configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './assets/css/style.css';
 
 const App = () => {
   return (
-    <div>
+    <div className={'bg-gray-600 text-white p-5 border'}>
       hello
     </div>
   );
