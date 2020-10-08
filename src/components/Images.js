dbd88df4618d757b5dc78e7ea6e0d47b847395a0
Salman Zafar <salman.zafar@innowi.com>
2020-10-08 15:06:32 +0500

    component inside component usage

--- src/components/Images.js
+++ src/components/Images.js
@@ -12,5 +12,25 @@ export const Images = () => {
 
     },[]);
 
-    return <img src={"https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"} alt={''}/>
+
+    const images = [
+        "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    ];
+
+    function ShowImage () {
+        return images.map(image => {
+            return <div>
+                <img src={image} width={'150'}/>
+            </div>
+        })
+    }
+
+    return <section>
+      <div className="flex justify-center">
+          <ShowImage/>
+      </div>
+    </section>
 }
