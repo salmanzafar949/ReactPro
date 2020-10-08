285bd546232619bc9a38129eb3a967883ae04d21
Salman Zafar <salman.zafar@innowi.com>
2020-10-08 17:43:21 +0500

    onMouseLeave and onMouseEnter events

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react";
 export const Images = () => {
 
     const [url, setUrl] = useState('');
+    const [isHovering, setIsHovering] = useState(-1);
     const [images, setImages] = useState([
         "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     ]);
@@ -27,11 +28,17 @@ export const Images = () => {
     function ShowImage () {
         return images.map((image, index) => {
             return <div className='w-1/3 my-4 flex justify-center' key={index}>
-                <div className="relative">
-                    <i className='fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100' onClick={() => {
-                        setImages(images.filter((image, i) => i !== index))
-                    }}/>
-                    <img src={image} width={'150'} alt={image}/>
+                <div className="relative"
+                     onMouseEnter={() => setIsHovering(index)}
+                     onMouseLeave={() => setIsHovering(-1)}>
+                    {
+                        isHovering === index ? <i className='fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100' onClick={() => {
+                            setImages(images.filter((image, i) => i !== index))
+                        }}/> : null
+                    }
+                    <img src={image}
+                         width={'150'}
+                         alt={image}/>
                 </div>
             </div>
         })
