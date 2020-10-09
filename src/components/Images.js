51ac5d74414e5d1392ea7d1f5b01807dc6ca08a3
Salman Zafar <salman.zafar@innowi.com>
2020-10-09 14:06:19 +0500

    code formatting

--- src/components/Images.js
+++ src/components/Images.js
@@ -4,6 +4,7 @@ import {ImageDisplay} from "./ImageDisplay";
 export const Images = () => {
 
     const [url, setUrl] = useState('');
+
     const [images, setImages] = useState([
         "https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     ]);
@@ -32,7 +33,6 @@ export const Images = () => {
     function ShowImage () {
         return images.map((image, index) => <ImageDisplay key={index} image={image} index={index} handleImageRemove={handleImageRemove}/>)
     }
-
 
     function FormComp () {
 
