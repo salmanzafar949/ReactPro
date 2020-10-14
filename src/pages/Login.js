a825043e95e72ea335cece2a2a5bbc8d7acef147
Salman Zafar <salman.zafar@innowi.com>
2020-10-14 13:58:33 +0500

    Login Form ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 const Login = () => {
-    return <div className="flex h-screen">
-        <h1 className="m-auto text-5xl">Login</h1>
+    return <div className="flex h-screen bg-white">
+        <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-600">
+            <form className="m-5 w-10/12">
+                <h1 className="w-full text-4xl tracking-widest text-center my-6"> Login </h1>
+                <div className="w-full my-6">
+                    <input className="p-2 rounded shadow w-full"
+                           type="email"
+                           placeholder="Email"/>
+                </div>
+                <div className="w-full my-6">
+                    <input className="p-2 rounded shadow w-full"
+                           type="password"/>
+                </div>
+                <div className="w-full my-10">
+                    <button type="submit" className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 }
 
