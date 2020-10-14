8c8ce00a60e7e21fcb3e0b78a95704ad29274ad5
Salman Zafar <salman.zafar@innowi.com>
2020-10-14 16:08:31 +0500

    ui changes in Login comp

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
         <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-600">
             <form onSubmit={handleLogin} className="m-5 w-10/12">
                     {
-                        error && <p> {error} </p>
+                        error && <p className="text-red-600 text-3xl"> {error} </p>
                     }
                 <h1 className="w-full text-4xl tracking-widest text-center my-6"> Login </h1>
                 <div className="w-full my-6">
