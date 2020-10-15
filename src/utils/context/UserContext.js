f6fa38e177f502354098bc32992e1d2009be1cb3
Salman Zafar <salman.zafar@innowi.com>
2020-10-15 14:44:01 +0500

    fixed loading issue of state

--- src/utils/context/UserContext.js
+++ src/utils/context/UserContext.js
@@ -1,31 +1,37 @@
 import React, {createContext, useEffect, useState} from "react";
 import firebase from "../../config/firebase";
+import Loader from "../../components/Loader";
 
 export const UserContext = createContext();
 
 
 export const UserProvider = (props) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(null);
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
+        setIsLoading(true)
         firebase.auth().onAuthStateChanged(user => {
             if (user)
             {
                 setUser(user);
                 setIsLoggedIn(true)
+                setIsLoading(false)
             }
             else
             {
                 setUser({});
                 setIsLoggedIn(false)
+                setIsLoading(false)
             }
         })
     },[])
 
-    return <UserContext.Provider value={[isLoggedIn, setIsLoggedIn, user, setUser]}>
+    return <UserContext.Provider value={[isLoggedIn, setIsLoggedIn, user, setUser, isLoading, setIsLoading]}>
         {
-            props.children
+           isLoading ? <Loader/> : props.children
         }
     </UserContext.Provider>
 }
