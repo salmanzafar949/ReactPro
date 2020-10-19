7d6fe4dc9932a298e55e5970a2b4e62bb1b17294
Salman Zafar <salman.zafar@innowi.com>
2020-10-19 17:04:51 +0500

    working on framer motion library

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,20 +1,24 @@
 import React, {useState} from "react";
+import {motion} from "framer-motion";
 
 export const ImageDisplay = ({image, index, handleImageRemove}) => {
 
     const [isHovering, setIsHovering] = useState(false);
 
-    return <div className='w-1/3 p-1 border flex justify-center'>
+    return <div className='w-1/5 p-1 border flex justify-center'>
         <div className="relative"
              onMouseEnter={() => setIsHovering(true)}
              onMouseLeave={() => setIsHovering(false)}>
             {
                 isHovering ? <i className='fas fa-times absolute right-0 cursor-pointer opacity-25 hover:opacity-100' onClick={() => handleImageRemove(index)}/> : null
             }
-            <img src={image}
-                 width="100%"
-                 height="auto"
-                 alt={image}/>
+            <motion.img src={image}
+                        whileHover={{
+                            scale: 2
+                        }}
+                        width="100%"
+                        height="auto"
+                        alt={image}/>
         </div>
     </div>
 }
